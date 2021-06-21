0d98f74d53d12c2284eec103c927d4d4524d1134
NoraElBakkali <84719421+NoraElBakkali@users.noreply.github.com>
2021-06-21 13:38:00 +0200

    Add files via upload

---
+++ Opdracht eerste JS/script.js
@@ -0,0 +1,19 @@
+console.log("Hello Winc Academy");
+let name = 'Nora';
+console.log(name);
+// if niet mogelijk omdat dat al een reseverd keywoord is.
+let addition = "4" + "4";
+console.log(addition);
+// moest code van 4+4 verwijderen om bovenstaande code te runnen. Getallen worden dus opgeteld en cijfers als string worden samen gevormd.
+let substraction = 905 - 34;
+console.log(substraction);
+let multiplication = 5 * 5;
+console.log(multiplication);
+let division = 30 / 6;
+console.log(division);
+//let age = 48;
+//console.log(typeof 48);
+//uitkomst number
+let age = "48"
+console.log(typeof "48");
+//uitkomst string
